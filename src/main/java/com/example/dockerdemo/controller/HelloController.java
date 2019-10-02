66d32e3d5d1f41f05b83aedb2be792ffcd5c9a91
jun.li <lijun359714923@163.com>
2019-10-02 22:58:50 +0800

    add hello controller

---
+++ src/main/java/com/example/dockerdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello docker demo";
+    }
+}
