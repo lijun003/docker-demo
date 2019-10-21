6e505a871c305a2d45d2fde3ea2a3fc61ce5b622
jun.li <lijun359714923@163.com>
2019-10-21 21:23:35 +0800

    modify kube yaml

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ ADD target/docker-demo-0.0.1-SNAPSHOT.jar /app.jar
 ENV TZ Asia/Shanghai
 RUN bash -c 'touch /app.jar'
 EXPOSE 8888
-ENTRYPOINT ["java","-Xmx400m","-Xms200m","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+
+ENTRYPOINT exec java -Djava.security.egd=file:/dev/./urandom  $JAVA_OPTS -jar /app.jar $RUN_ARGS
--- src/main/java/com/example/dockerdemo/controller/HelloController.java
+++ src/main/java/com/example/dockerdemo/controller/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.dockerdemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("hello")
 @Slf4j
 public class HelloController {
+    @Value("${env}")
+    private String env;
 
     @GetMapping
     public String sayHello() {
-        log.info("Calling hello controller! at {}", System.currentTimeMillis());
-        return "hello docker demo";
+        log.info("Calling hello controller! at {}, on {}", System.currentTimeMillis(), env);
+        return "hello docker demo at " + env;
     }
 }
